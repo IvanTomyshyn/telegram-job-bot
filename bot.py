957d030cb72901aeafa9ac308c6fe44a880f8e55
Ivan Tomyshyn <praceczukr@gmail.com>
2025-07-18 14:05:40 +0200

    Fix Flask host and port for Render

--- bot.py
+++ bot.py
@@ -6,4 +6,6 @@ def hello():
     return "Telegram Job Bot - simple test"
 
 if __name__ == "__main__":
-    app.run()
+    import os
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
